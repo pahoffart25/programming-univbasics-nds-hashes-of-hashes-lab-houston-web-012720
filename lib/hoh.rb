615475fc5359b8316898fc675d7efffedd697d1d
Paul Hoffart <pahoffart25@gmail.com>
2019-12-10 17:56:54 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -55,9 +55,7 @@ naming = {
                label: "Genus",
                sub_category: {
                  label: "Species",
-                 sub_category:nil {
-
-                 }
+                 sub_category: nil 
                }
            }
          }
